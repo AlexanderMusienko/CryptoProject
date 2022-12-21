6d3cd25c3f8fc0009c7e9310f6906d62060c4cf7
AlexanderMusienko <101179294+AlexanderMusienko@users.noreply.github.com>
2022-12-22 02:50:16 +0300

    bottom navbar work in progress

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,21 +1,24 @@
-import { Tab, Tabs } from "@chakra-ui/react";
+import { Stack, Tab, Tabs } from "@chakra-ui/react";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ExchangeDesk from "../ExchangeDesk";
-import './style.scss'
+import "./style.scss";
 
 export default function HomePage() {
   return (
-    <>
-      <Routes>
-        <Route index element={<ExchangeDesk />} />
-        <Route path="exchange-desk" element={<ExchangeDesk />} />
-      </Routes>
-      <Tabs variant={'unstyled'} className="bottom-navbar">
+    <Stack display={'grid'} gridTemplateRows={'1fr 0.1fr'} className="main-page-wrapper">
+      <Stack className="main-page-container">
+        <Routes>
+          <Route shouldRevalidate={true} index element={<ExchangeDesk />} />
+          <Route path="exchange-desk" element={<ExchangeDesk />} />
+        </Routes>
+      </Stack>
+      <Tabs colorScheme={{__selected: 'red.400'}} className="bottom-navbar">
         <Tab>Home</Tab>
         <Tab>P2P</Tab>
+        <Tab>Currency rate</Tab>
         <Tab>Profile</Tab>
       </Tabs>
-    </>
+    </Stack>
   );
 }
