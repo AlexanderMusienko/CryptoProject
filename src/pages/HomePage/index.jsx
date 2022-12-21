0130f0966ff8cda7e5fc54d1bf01d03bcd6df3a7
AlexanderMusienko <101179294+AlexanderMusienko@users.noreply.github.com>
2022-12-22 02:50:38 +0300

    bottom navbar work in progress

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,14 +6,14 @@ import "./style.scss";
 
 export default function HomePage() {
   return (
-    <Stack display={'grid'} gridTemplateRows={'1fr 0.1fr'} className="main-page-wrapper">
+    <Stack display={"grid"} gridTemplateRows={"1fr 0.1fr"} className="main-page-wrapper">
       <Stack className="main-page-container">
         <Routes>
           <Route shouldRevalidate={true} index element={<ExchangeDesk />} />
           <Route path="exchange-desk" element={<ExchangeDesk />} />
         </Routes>
       </Stack>
-      <Tabs colorScheme={{__selected: 'red.400'}} className="bottom-navbar">
+      <Tabs className="bottom-navbar">
         <Tab>Home</Tab>
         <Tab>P2P</Tab>
         <Tab>Currency rate</Tab>
