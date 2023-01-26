fe6f90d82bb61a5f88bdb4c1d21861887d2c13e7
Alexander Musienko <alexmvid0@gmail.com>
2023-01-26 09:51:53 +0300

    added react-select styled select in estimatedBalance component

--- src/pages/HomePage/EstimatedBalance/index.jsx
+++ src/pages/HomePage/EstimatedBalance/index.jsx
@@ -3,6 +3,21 @@ import { useState } from "react";
 import { HStack, Wrap, Text } from "@chakra-ui/react";
 import Select from "react-select";
 
+const selectStyles = {
+  valueContainer: (baseStyles) => ({
+    ...baseStyles,
+    display: "none"
+  }),
+  indicatorSeparator: (baseStyles) => ({
+    ...baseStyles,
+    display: "none"
+  }),
+  menu: (baseStyles) => ({
+    ...baseStyles,
+    width: '70px'
+  })
+}
+
 /**
  *
  * @param {Object} props
@@ -14,8 +29,8 @@ import Select from "react-select";
 export default function EstimatedBalance({ estimatedBalanceCoins, estimatedBalanceFiats, coinOptions }) {
   const [coin, setCoin] = useState("btc");
 
-  function getOptionValue(e) {
-    setCoin(e.target.value);
+  function setOptionValue(e) {
+    setCoin(e.value);
   }
 
   const balance = {
@@ -28,18 +43,14 @@ export default function EstimatedBalance({ estimatedBalanceCoins, estimatedBalan
       <Text className="coin-balance">
         {balance.coinBalance.hot} {balance.coinBalance.coin.toUpperCase()}
       </Text>
-      <Wrap className="coin-select">
         <Select
-          // options={coinOptions.map((coin, i) => {
-          //   return { value: coin, label: coin };
-          // })}
-          options={[
-            { value: 'chocolate', label: 'Chocolate' },
-            { value: 'strawberry', label: 'Strawberry' },
-            { value: 'vanilla', label: 'Vanilla' }
-          ]}
-        />
-      </Wrap>
+        className="coin-select"
+        styles={selectStyles}
+        onChange={setOptionValue}
+        options={coinOptions.map((coin, i) => {
+          return { value: coin, label: coin.toUpperCase() };
+        })}
+      />
       <Text className="fiat-balance">
         ≈ {balance.fiatBalance.hot} {balance.fiatBalance.fiat.toUpperCase()}
       </Text>
