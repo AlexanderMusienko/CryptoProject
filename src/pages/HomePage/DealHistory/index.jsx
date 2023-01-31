7c9b2e6f7b5bcae517f0bbc7f97b7ed17335d1d0
Alexander Musienko <alexmvid0@gmail.com>
2023-01-31 14:45:32 +0300

    dealHistory refactoring

--- src/pages/HomePage/DealHistory/index.jsx
+++ src/pages/HomePage/DealHistory/index.jsx
@@ -1,4 +1,4 @@
-import { Badge, HStack, Stack, Text } from "@chakra-ui/react";
+import { Badge, HStack, Stack, Text, Box } from "@chakra-ui/react";
 import React from "react";
 import defineColorScheme from "../../../utils/defineColorScheme";
 
@@ -8,22 +8,43 @@ const badgeConfig = {
   gray: "processing",
 };
 
+const dateOptions = {
+  month: "long",
+  day: "numeric",
+  year: "numeric",
+};
+
 export default function DealHistory({ dealHistories }) {
-  const DealItem = ({ coin, dealType, fiat, orderAmount, dealStatus, executorName, executorId }) => (
+  const DealItem = ({
+    coin,
+    dealType,
+    fiat,
+    orderAmount,
+    dealStatus,
+    executorName,
+    executorId,
+    deliveryDate,
+  }) => (
     <HStack className="deal-item">
-      <Text className="transaction-type">
-        {coin.toUpperCase()} {dealType.toUpperCase()}
-      </Text>
-      <Text className="order-amount">
-        {fiat}
-        {orderAmount}
-      </Text>
-      <Badge className="delivery-status" colorScheme={defineColorScheme(dealStatus, badgeConfig)}>
-        {dealStatus}
-      </Badge>
-      <Text className="executor-info">
-        {executorName} {executorId}
-      </Text>
+      <HStack className="deal-info-container">
+        <Text className="coin">{coin.toUpperCase()}</Text>
+        <Box>
+          <Text className="deal-type">{dealType.toUpperCase()}</Text>
+          <Text className="delivery-date">{deliveryDate.toLocaleString("en-US", dateOptions)}</Text>
+        </Box>
+      </HStack>
+
+      <Box className="amount-status-container">
+        <Text className="order-amount" color={dealType === 'sell' ? 'green' : ''}>
+          {dealType === "sell" ? "+" : "-"}
+          {' '}
+          {fiat}
+          {orderAmount}
+        </Text>
+        <Badge className="delivery-status" colorScheme={defineColorScheme(dealStatus, badgeConfig)}>
+          {dealStatus}
+        </Badge>
+      </Box>
     </HStack>
   );
 
